4c52a9219f312ddc2f85fb1971f9859f8d6ccf4e
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-26 22:37:57 +0000

    Update article

--- routes/articles.js
+++ routes/articles.js
@@ -33,7 +33,8 @@ router.post('/', asyncHandler(async (req, res) => {
 
 /* Edit article form. */
 router.get("/:id/edit", asyncHandler(async(req, res) => {
-  res.render("articles/edit", { article: {}, title: "Edit Article" });
+  const article = await Article.findByPk(req.params.id);
+  res.render("articles/edit", { article, title: "Edit Article" });
 }));
 
 /* GET individual article. */
@@ -44,7 +45,9 @@ router.get("/:id", asyncHandler(async (req, res) => {
 
 /* Update an article. */
 router.post('/:id/edit', asyncHandler(async (req, res) => {
-  res.redirect("/articles/");
+  const article = await Article.findByPk(req.params.id);
+  await article.update(req.body);
+  res.redirect("/articles/" + article.id);
 }));
 
 /* Delete article form. */
