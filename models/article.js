01f024ec8c38e9386aca1ad1863516351f570b0d
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-26 22:25:35 +0000

    Format created at date

--- models/article.js
+++ models/article.js
@@ -1,8 +1,13 @@
 'use strict';
 const Sequelize = require('sequelize');
+const moment = require('moment');
 
 module.exports = (sequelize) => {
-  class Article extends Sequelize.Model {}
+  class Article extends Sequelize.Model {
+    publishedAt() {
+      const date = moment(this.createdAt).format('MMMM D, YYYY, h:mma');
+      return date;
+    }
   Article.init({
     title: Sequelize.STRING,
     author: Sequelize.STRING,
