30669680554173980124bbbb2cc6f3f5a34e0768
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-26 22:25:46 +0000

    Format description display

--- models/article.js
+++ models/article.js
@@ -8,6 +8,11 @@ module.exports = (sequelize) => {
       const date = moment(this.createdAt).format('MMMM D, YYYY, h:mma');
       return date;
     }
+    shortDescription() {
+      const shortDesc = this.body.length > 200 ? this.body.substring(0 ,200) + '...' : this.body;
+      return shortDesc;
+    }
+  }
   Article.init({
     title: Sequelize.STRING,
     author: Sequelize.STRING,
